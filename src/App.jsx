18b6f73fa14f52d0083d31943c618f881cbd9604
hayliepedersen <pedersenhaylie6@gmail.com>
2024-08-16 21:55:21 -0400

    added api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
   const RESPONSE_TYPE = "token"
 
   const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
 
   useEffect(() => {
     const hash = window.location.hash
@@ -29,6 +32,31 @@ function App() {
     window.localStorage.removeItem("token")
   }
 
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const token = window.localStorage.getItem("token")
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+
+    setArtists(data.artists.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+  }
+
   return (
     <>
      <div className="App">
@@ -39,6 +67,13 @@ function App() {
             : <button onClick={logout}>Logout</button>
           }
         </header>
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search</button>
+        </form>
+        <div>
+          {renderArtists()}
+        </div>
       </div>
     </>
   )
