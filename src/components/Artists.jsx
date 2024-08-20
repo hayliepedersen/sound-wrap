2b2b3dcfe7f5455d47a453d6b10b8fbbe436012c
hayliepedersen <pedersenhaylie6@gmail.com>
2024-08-20 13:40:12 -0400

    improved file organization

---
+++ src/components/Artists.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import axios from 'axios'
+
+function Artists() {
+  const [searchKey, setSearchKey] = useState("");
+  const [artists, setArtists] = useState([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // Search artists based off of query
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const token = window.localStorage.getItem("token")
+
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+
+    setArtists(data.artists.items)
+  }
+
+  // Handles the submittion of user input
+  const handleSubmit = (e) => {
+    searchArtists(e)
+    setIsSubmitted(true)
+  }
+
+  // Display the reccomended artists to the user
+  const renderArtists = () => {
+    return artists.slice(0, 8).map(artist => (
+        <div key={artist.id}>
+            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+            {artist.name}
+        </div>
+    ))
+  }
+
+  return (
+    <>
+      <div className="Artists">
+        <h2>Artist Reccomendation</h2>
+        {!isSubmitted && <p>Enter your favorite artist:</p>}
+        <form onSubmit={handleSubmit} id="searchArtists">
+          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+          <button type={"submit"}>Search</button>
+        </form>
+        {isSubmitted && <p>You may also like: </p>}
+        <div className="artistImages">
+          {renderArtists()}
+         </div>
+      </div>
+    </>
+  )
+}
+
+export default Artists;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,9 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios'
+import Artists from './Artists'
 
 function Home() {
-  const [searchKey, setSearchKey] = useState("");
-  const [artists, setArtists] = useState([]);
   const [playlists, setPlaylists] = useState([]);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  // Search artists based off of query
-  const searchArtists = async (e) => {
-    e.preventDefault()
-    const token = window.localStorage.getItem("token")
-
-    const {data} = await axios.get("https://api.spotify.com/v1/search", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-      params: {
-        q: searchKey,
-        type: "artist"
-      }
-    })
-
-    setArtists(data.artists.items)
-  }
-
-  const handleSubmit = (e) => {
-    searchArtists(e)
-    setIsSubmitted(true)
-  }
-
-  // Display the reccomended artists to the user
-  const renderArtists = () => {
-    return artists.slice(0, 8).map(artist => (
-        <div key={artist.id}>
-            {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-            {artist.name}
-        </div>
-    ))
-  }
-
-  // -----------------
 
   // Get all of the user's playlists
   const getPlaylists = async () => {
@@ -108,21 +71,12 @@ function Home() {
   return (
     <>
       <div className="Home">
-        <h2>Artist Reccomendation</h2>
-        {!isSubmitted && <p>Enter your favorite artist:</p>}
-        <form onSubmit={handleSubmit} id="searchArtists">
-          <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-          <button type={"submit"}>Search</button>
+        < Artists/>
+        {/*Currently only returns playlists to console*/}
+        <form onSubmit={getPlaylists} id="getPlaylists">
+          <button type={"submit"}>Get Playlists</button>
         </form>
-        {isSubmitted && <p>You may also like: </p>}
-        <div className="artistImages">
-          {renderArtists()}
-        </div>
-          {/*Currently only returns playlists to console*/}
-          <form onSubmit={getPlaylists} id="getPlaylists">
-            <button type={"submit"}>Get Playlists</button>
-          </form>
-        </div>
+      </div>
     </>
   )
 }
