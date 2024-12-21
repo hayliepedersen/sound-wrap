978d99358d8c6f8359ccdeffa396b37351ab8e90
hayliepedersen <pedersenhaylie6@gmail.com>
2024-12-20 22:06:15 -0500

    updated authentication

--- src/App.jsx
+++ src/App.jsx
@@ -25,15 +25,19 @@ function App() {
     const hash = window.location.hash
     let storedToken = window.localStorage.getItem("token")
 
-    if (!token && hash) {
+    if (hash) {
       storedToken = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"))?.split("=")[1]
 
-      window.location.hash = ""
-      window.localStorage.setItem("token", storedToken)
+      if (storedToken) {
+        window.location.hash = ""
+        window.localStorage.setItem("token", storedToken)
+        setToken(storedToken)
+        setIsAuthenticated(true)
+      }
+    } else if (storedToken) {
+      setToken(storedToken)
       setIsAuthenticated(true)
     }
-
-    setToken(storedToken)
   }, [])
 
   const logout = () => {
